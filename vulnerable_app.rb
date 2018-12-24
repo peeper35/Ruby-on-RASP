69b42b7ac1a087b448508960aae072c64cca60d4
Sahil Tembhare <isr.sahil.mk@gmail.com>
2018-12-24 23:00:24 +0530

    Added the vulnerable app file

---
+++ vulnerable_app.rb
@@ -0,0 +1,55 @@
+require './controller'
+require 'haml'
+
+class Application < ApplicationController
+
+  	['/', '/index'].each do |path|
+		get path  do 
+			haml :index
+ 		end
+ 	end
+
+	post '/upload' do
+		@filename = params[:file][:filename]
+		file = params[:file][:tempfile]
+
+		File.open("./public/uploads/#{@filename}", 'wb') do |f|
+			f.write(file.read)
+		end
+
+		haml :upload_successful
+	end	
+
+	get '/read' do 
+		self.readfile(params[:file])
+	end
+
+	get '/server' do
+		@server = "127.0.0.1"
+		
+		haml :server
+	end
+
+	post '/server' do
+		@msg = doping(params[:server])
+		@server = params[:server]
+		
+		haml :server
+	end
+
+	get '/search' do
+		@search_query = params[:q]
+		
+		haml :search
+	end
+
+	get '/login' do
+		haml :login
+	end
+
+	post '/login' do
+		@app = self.checklogin("#{params[:username]}", "#{params[:password]}")
+
+		haml :login
+	end
+end
