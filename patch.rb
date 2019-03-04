2a3b67898b5c79993d506bbd6a151e2e6cbf8214
Sahil Tembhare <isr.sahil.mk@gmail.com>
2019-03-04 23:52:28 +0530

    Added sqli patch and rules thingy

--- patch.rb
+++ patch.rb
@@ -1,10 +1,34 @@
 require 'htmlentities'
+require 'sqlite3'
+require 'colorize'
+require './lexer'
+require './controller'
 
 
 class Patch
 	def patchxss(sq)
 		encoder = HTMLEntities.new
 		encoder.encode(sq)	
+	end
+
+    def patchsqli(uname, pwd)
+        @un = uname
+        @pw = pwd
+		lex = takein("SELECT * FROM logins WHERE username ='" + @un + "' AND password ='" + @pw + "'")
+			def check(lex, uname=@un, pwd=@pw)
+				db = SQLite3::Database.open("./logindata.db")
+				data = db.execute("SELECT * FROM sqli_rules WHERE sqlirule ='" + lex + "'")
+
+                if !data.empty?
+                    puts "#{lex.colorize(:green)}\n"
+                    return checklogin(uname, pwd)
+                else
+                    puts "#{lex.colorize(:red)}\n"
+		    	    return "ThisSpecifiedStringSQLi"
+                end
+			end
+
+		check(lex)
 	end
 end
 
