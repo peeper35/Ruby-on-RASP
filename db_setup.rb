283ceb422dfbfffda2171d9ac7f101bb185cfb48
Sahil Tembhare <isr.sahil.mk@gmail.com>
2019-03-04 23:56:31 +0530

    Added a table to the database with sql rule

--- db_setup.rb
+++ db_setup.rb
@@ -26,3 +26,21 @@ db.execute( "select * from logins") do |row|
 end
 
 puts "\nDatabase Created!"
+
+
+
+rows = db.execute <<-SQL
+  create table sqli_rules (
+    sqlirule varchar(1000)
+  );
+SQL
+
+sqli = "[[STRING], [WHITESPACE], [OPERATOR], [WHITESPACE], [STRING], [WHITESPACE], [STRING], [WHITESPACE], [STRING], [WHITESPACE], [STRING], [WHITESPACE], [EQUALS], [SINGLEQ], [STRING], [SINGLEQ], [WHITESPACE], [STRING], [WHITESPACE], [STRING], [WHITESPACE], [EQUALS], [SINGLEQ], [STRING], [SINGLEQ]]"
+
+db.execute "insert into sqli_rules ( sqlirule ) values ( ? )", [sqli]
+
+db.execute("select * from sqli_rules") do |row|
+  p row
+end
+
+puts "\nSQLi Rule Added!"
